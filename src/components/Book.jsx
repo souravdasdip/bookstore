acb87bc5ed665f6c810fae133e814904998d0586
Sourav Das <souravdasdip65@gmail.com>
2023-06-05 01:21:30 +0600

    Update Book.jsx

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -48,21 +48,6 @@ const Book = ({ book }) => {
                 <path d="M12 5l7 7-7 7"></path>
               </svg>
             </a>
-            <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm">
-              <svg
-                class="w-4 h-4 mr-1"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                viewBox="0 0 24 24"
-              >
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>
-              {book?.download_count}
-            </span>
           </div>
         </div>
       </div>
